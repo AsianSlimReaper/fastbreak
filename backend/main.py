878fe45ea58ed519012d76a29a5233d8f501409f
kevin <kevinyu6666@gmail.com>
2025-08-21 22:58:21 +1000

    finished internal doc for backend

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from backend.api.routes import user, teams, dashboard, stats, player_profile,film_room,wasabi
 from fastapi.middleware.cors import CORSMiddleware
 
+# FastAPI application setup
 app = FastAPI()
 
+
+# Include routers for different API endpoints
 app.include_router(user.router)
 app.include_router(teams.router)
 app.include_router(dashboard.router)
@@ -12,6 +15,7 @@ app.include_router(player_profile.router)
 app.include_router(film_room.router)
 app.include_router(wasabi.router)
 
+# CORS middleware configuration
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
